9c6826a1910cbb91cff132e7401ead94cc67882c
Linzh <catmo1999@gmail.com>
2022-09-30 21:32:09 +0300

    fix the process bug

--- datasetprocess.py
+++ datasetprocess.py
@@ -1,6 +1,7 @@
 import os
 import LinzhUtil
 from sklearn.model_selection import train_test_split
+from tqdm import tqdm
 
 path = './dataset/'
 
@@ -8,11 +9,12 @@ file_list = LinzhUtil.getFileList('./dataset/')
 
 with open('./train.csv', 'w') as train_file, open('./test.csv',
                                                   'w') as test_file:
-    for file in file_list:
+    for file in tqdm(file_list):
         if file.endswith('.csv'):
             with open(os.path.join(path, file), 'r') as read_file:
-                records = read_file.readlines()
-                records[-1] += '\n'
+                records = list(
+                    filter(lambda x: x != '\n' and len(x.split(',')) == 127,
+                           read_file.readlines()))
                 train, test = train_test_split(records, test_size=0.25)
                 train_file.writelines(train)
                 test_file.writelines(test)
